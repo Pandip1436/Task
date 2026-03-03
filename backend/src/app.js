610e104a6b0436526747f84140468c47524a5c68
Muthu Pandi <pandip1436@gmail.com>
2026-03-03 12:36:23 +0530

    Update

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.use(
 );
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://task-855.pages.dev/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
