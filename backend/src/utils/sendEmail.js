da1807762a22f5681428a35ca2f6d4f3e7174d4d
Muthu Pandi <pandip1436@gmail.com>
2026-03-04 17:10:58 +0530

    Update

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,10 +1,14 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // required for port 465
+    port: 587,
+    secure: false,
+    family: 4, // force IPv4
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
